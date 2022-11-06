7423eef07aff354d0ef9872dc2985938f8edb16a
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-06 22:46:53 +0530

    setup server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -6,3 +6,22 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once("open", ()=>{
+    console.log("MongoDB Connection Success!");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running on PORT : ${PORT}`);
+})
