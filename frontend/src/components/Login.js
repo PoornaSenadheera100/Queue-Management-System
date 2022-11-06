6cf1bc0a5164a9ffa5e649f90a6c2c8a7bb072a6
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-06 17:59:10 +0530

    Added basic variables

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validate(){
+        console.log(username, password);
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={validate}>
                 Username
-                <input type="text" placeholder="Username"></input>
+                <input type="text" placeholder="Username" onChange={(e)=>{
+                    setUsername(e.target.value);
+                }}></input>
+                <br></br>
                 Password
-                <input type="password" placeholder="Password"></input>
+                <input type="password" placeholder="Password" onChange={(e)=>{
+                    setPassword(e.target.value);
+                }}></input>
+                <button type="submit">Login</button>
             </form>
         </div>
     )
