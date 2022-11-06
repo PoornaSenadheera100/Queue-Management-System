c87eba62b9085520e0a4edb84bc78e074868166b
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-06 17:40:41 +0530

    Created Login component

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Login(){
+    return(
+        <div>
+            <form>
+                Username
+                <input type="text" placeholder="Username"></input>
+                Password
+                <input type="password" placeholder="Password"></input>
+            </form>
+        </div>
+    )
+}
