7e38ae77e567e30b99524f2a89d66b03d65ae24d
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-06 18:31:21 +0530

    Applied css

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -10,18 +10,28 @@ export default function Login(){
     }
 
     return(
-        <div>
-            <form onSubmit={validate}>
-                Username
-                <input type="text" placeholder="Username" onChange={(e)=>{
-                    setUsername(e.target.value);
-                }}></input>
+        <div className="text-center container d-flex justify-content-center p-3 mb-2 bg-dark text-white" style={{width:"50%"}}>
+            <form className="form-signin" onSubmit={validate}>
+                <h2>Queue Management Portal</h2>
                 <br></br>
-                Password
-                <input type="password" placeholder="Password" onChange={(e)=>{
-                    setPassword(e.target.value);
-                }}></input>
-                <button type="submit">Login</button>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" placeholder="Username" className="form-control" onChange={(e)=>{
+                        setUsername(e.target.value);
+                    }}></input>
+                </div>
+                <br></br>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" placeholder="Password" className="form-control" onChange={(e)=>{
+                        setPassword(e.target.value);
+                    }}></input>
+                </div>
+                <br></br>
+                <br></br>
+                <button style={{width:"100%"}} className="btn btn-lg btn-success btn-block" type="submit">Login</button>
+                <br></br>
+                <br></br>
             </form>
         </div>
     )
