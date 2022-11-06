453cd8dee10b12f487eca2ee0557edd9a8228ce2
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-06 19:09:22 +0530

    Added properties

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
