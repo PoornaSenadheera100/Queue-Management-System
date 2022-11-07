9c3f97decaf36cba45e8c6b35fcd5f09b4afe157
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-07 09:51:08 +0530

    Added route for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Header from "./components/Header";
 import Login from "./components/Login";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Header />,
+      errorElement: <Login/>,
+      children: [
+        {
+          path: "login",
+          element: <Login />
+        }
+      ]
+    }
+  ]);
+
   return (
-    <div>
-      <Login/>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
