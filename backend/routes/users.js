7a6194580d50d51806b5855c8e5d72ca0efe5c81
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-07 21:26:47 +0530

    Added add method for users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,2 +1,20 @@
 const router = require("express").Router();
 let User = require("../models/User");
+
+router.route("/add").post((req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const newUser = new User({
+        username,
+        password
+    })
+
+    newUser.save().then(()=>{
+        res.json("User Added.");
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+module.exports = router;
