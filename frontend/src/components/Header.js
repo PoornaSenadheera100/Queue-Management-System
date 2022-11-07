44b2378d4e8cd4e6bb38ce3b0889c87b90ae50d3
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-07 09:51:33 +0530

    Added outlet in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Header(){
     return(
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <>
+            <div>
+                <h1>This is Header</h1>
+                <Link to={'login'}>Login</Link>
+            </div>
+
+            <div id="detail">
+                <Outlet/>
+            </div>
+        </>
     )
 }
