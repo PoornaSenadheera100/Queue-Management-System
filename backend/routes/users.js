ba8cb856f458ddcf8c409caedf814a2228d60140
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-11-10 23:14:21 +0530

    Added validate backend route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -27,5 +27,14 @@ router.route("/get").get((req, res)=>{
 })
 
 
+router.route("/validate/:username").get(async(req, res)=>{
+    let username = req.params.username;
+    await User.find({"username" : `${username}`}).then((user)=>{
+        res.json(user);
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status: "Error with get the user details", error: err.message});
+    })
+})
 
 module.exports = router;
